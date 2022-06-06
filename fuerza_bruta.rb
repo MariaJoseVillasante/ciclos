c94f998a5287976ac5501c6d72d6890cccbad237
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-06-06 13:22:27 -0400

    fuerza bruta

---
+++ fuerza_bruta.rb
@@ -0,0 +1,13 @@
+#27 letras hay en el abcedario
+password = ARGV[0]
+largo = password.size
+intentos = largo
+nueva = "a"
+
+while password != nueva
+    nueva = nueva.next
+    intentos += 1
+    
+end
+
+puts "#{intentos} intentos"
