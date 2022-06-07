47b4d9b584b78ecbf4e47ad63c029567d6d378bc
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-06-07 12:49:33 -0400

    otros

--- iterador.rb
+++ iterador.rb
@@ -7,3 +7,8 @@
 50.times do |num|
     puts "Iteración #{num}"
 end
+
+5.times{|i|puts "Iteración #{i*2}"}
+2.upto(5){|i|puts "Iteración #{i*2}"}
+5.downto(1){puts "Iteración"}
+(1..5).each{puts"Iteración "}
