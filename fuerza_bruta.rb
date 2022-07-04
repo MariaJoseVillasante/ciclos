3243ba9d4ee84525b7203eb3b461b9cdd10a0725
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-07-04 16:29:45 -0400

    agregando ciclos y métodos para arreglar git

--- fuerza_bruta.rb
+++ fuerza_bruta.rb
@@ -3,11 +3,8 @@ nueva = "a"
 intentos = 0
 
 while password != nueva
-    #puts ","
     nueva = nueva.next
-    #puts nueva
-    intentos += 1
-    
+    intentos += 1  
 end
 
 puts "#{intentos} intentos"
